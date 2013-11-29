95635808303301b75d9577a686fe6e6371084b95
tonilin <tonilin@gmail.com>
2013-11-30 01:02:58 +0800

    init

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Geekcoffee::Application.config.session_store :cookie_store, key: '_geekcoffee_session'
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Geekcoffee::Application.routes.draw do
+  devise_for :users
+  root :to => 'high_voltage/pages#show', :id => 'welcome'
+end
