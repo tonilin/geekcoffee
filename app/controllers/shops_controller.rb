97b5171a7974e5dfa727f3fb176941be4ec3b37e
tonilin <tonilin@gmail.com>
2013-12-01 21:14:29 +0800

    shop create and new

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,7 +1,23 @@
 class ShopsController < ApplicationController
 
   def new
+    @shop = Shop.new
+  end
 
+  def create
+    @shop = Shop.new(shop_params)
+
+    if @shop.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def shop_params
+    params.require(:shop).permit(:name, :description, :hours, :is_wifi_free, :have_plugs, :plug_price)
   end
 
 
