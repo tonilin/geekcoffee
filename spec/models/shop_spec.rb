29abd43e0c8071965143a080175db81983811c30
tonilin <tonilin@gmail.com>
2013-12-01 20:23:15 +0800

    annotate

--- spec/models/shop_spec.rb
+++ spec/models/shop_spec.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: shops
+#
+#  id             :integer          not null, primary key
+#  name           :string(255)
+#  description    :text
+#  address        :string(255)
+#  format_address :string(255)
+#  hours          :string(255)
+#  lat            :decimal(10, 6)
+#  lng            :decimal(10, 6)
+#  is_wifi_free   :boolean
+#  have_plugs     :boolean
+#  plug_price     :integer
+#  created_at     :datetime
+#  updated_at     :datetime
+#
+
 require 'spec_helper'
 
 describe Shop do
