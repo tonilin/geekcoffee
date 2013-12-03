2a8a8408b9aa6ca238f241558c7725335ee2fe27
tonilin <tonilin@gmail.com>
2013-12-04 01:22:41 +0800

    style landing btn

--- app/helpers/maps_helper.rb
+++ app/helpers/maps_helper.rb
@@ -1,2 +1,8 @@
 module MapsHelper
+
+  def render_landing_maps_btn
+    link_to("開始尋找", maps_path, :class => "btn btn-primary btn-success btn-large")
+  end
+
+
 end
--- app/helpers/shops_helper.rb
+++ app/helpers/shops_helper.rb
@@ -1,8 +1,4 @@
 module ShopsHelper
 
 
-  def render_landing_maps_btn
-    link_to("尋找咖啡廳", maps_path, :class => "btn btn-primary")
-  end
-
 end
