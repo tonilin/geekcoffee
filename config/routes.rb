b8d9cd8deccff899d662f5cf5a5c1bd159f5ce34
tonilin <tonilin@gmail.com>
2013-12-04 10:33:09 +0800

    add omniauth callback controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Geekcoffee::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root :to => 'pages#welcome'
 
   resources :maps
