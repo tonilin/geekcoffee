29d6bf17ffc3b1953c81e0299306df816eef57a9
tonilin <tonilin@gmail.com>
2013-12-04 10:32:39 +0800

    add fb_id to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
+  extend OmniauthCallbacks
+  
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 end
