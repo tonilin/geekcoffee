165806c791b0e24bd995f86ab1578c3299b4ae72
tonilin <tonilin@gmail.com>
2013-12-10 23:26:26 +0800

    refine the btn

--- app/helpers/maps_helper.rb
+++ app/helpers/maps_helper.rb
@@ -1,7 +1,7 @@
 module MapsHelper
 
   def render_landing_maps_btn
-    link_to("開始尋找", maps_path, :class => "btn btn-primary btn-success btn-large")
+    link_to("開始尋找", maps_path, :class => "btn btn-primary btn-danger btn-lg")
   end
 
 
