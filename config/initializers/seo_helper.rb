ee995b0a3edb38c01b1ef79e8d6a4310c62f165a
tonilin <tonilin@gmail.com>
2013-12-12 23:30:17 +0800

    edit desc and keyword

--- config/initializers/seo_helper.rb
+++ config/initializers/seo_helper.rb
@@ -2,8 +2,8 @@
 SeoHelper.configure do |config|
   config.skip_blank               = false
   config.site_name = Setting.app_name
-  config.default_page_description = "尋找你附近的咖啡廳"
-  config.default_page_keywords    = "wifi,cafe,coffee,free"
+  config.default_page_description = "Geek Coffee, 尋找你附近的咖啡廳"
+  config.default_page_keywords    = "Geek, wifi, cafe, coffee, free"
   config.default_page_image = Setting.domain + Setting.default_logo_url
   config.site_name_formatter  = lambda { |title, site_name|   "#{title} « #{site_name}".html_safe }
   config.pagination_formatter = lambda { |title, page_number| "#{title} - Page No.#{page_number}" }
