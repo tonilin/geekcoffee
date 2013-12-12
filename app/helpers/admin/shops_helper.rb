4a4faa80d1d0fa3a979806ec287de9f9d11701f2
tonilin <tonilin@gmail.com>
2013-12-12 18:56:09 +0800

    add helpers

--- app/helpers/admin/shops_helper.rb
+++ app/helpers/admin/shops_helper.rb
@@ -1,2 +1,26 @@
 module Admin::ShopsHelper
+
+  def render_admin_shop_name(shop)
+    shop.name
+  end
+
+  def render_admin_shop_address(shop)
+    shop.address
+  end
+
+  def render_admin_shop_hours(shop)
+    shop.hours
+  end
+
+  def render_admin_shop_is_wifi_free(shop)
+    shop.is_wifi_free
+  end
+  
+  def render_admin_shop_power_outlets(shop)
+    shop.power_outlets
+  end
+
+
+
+
 end
---
+++ app/helpers/admin/users_helper.rb
@@ -0,0 +1,9 @@
+module Admin::UsersHelper
+
+  def render_admin_user_name(user)
+    user.name
+  end
+
+
+
+end
