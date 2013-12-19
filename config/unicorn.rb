f64415f04c945c081d4ec4a7da9f8e3866f27a1e
tonilin <tonilin@gmail.com>
2013-12-19 11:41:01 +0800

    change unicorn setting

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -13,7 +13,7 @@ shared_path = "/home/apps/geekcoffee/shared"
 
 # Use at least one worker per core if you're on a dedicated server,
 # more will usually help for _short_ waits on databases/caches.
-worker_processes 4
+worker_processes 2
 
 # Since Unicorn is never exposed to outside clients, it does not need to
 # run on the standard HTTP port (80), there is no reason to start Unicorn
@@ -29,7 +29,6 @@ working_directory current_path # available in 0.94.0+
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
 listen "#{shared_path}/sockets/unicorn.sock", :backlog => 64
-listen 8080, :tcp_nopush => true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 30
