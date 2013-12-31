1af13c7069ef68983d97d73a2968caab0548af6e
tonilin <tonilin@gmail.com>
2013-12-31 09:21:48 +0800

    add shops api

--- app/controllers/api/shops_controller.rb
+++ app/controllers/api/shops_controller.rb
@@ -1,5 +1,19 @@
 class Api::ShopsController < APIController
 
+  def index
+    per_page = (params[:per_page] || 100).to_i
 
-  
+    if per_page <= 0
+      return render :json => { :errors => "per_page should greater than zero" }, :status => 422
+    end
+
+    if per_page > 500
+      return render :json => { :errors => "per_page should less than 500" }, :status => 422
+    end
+
+    @shops = Shop.recent.paginate(:page => params[:page], :per_page => per_page )
+
+    return render :json => @shops, :each_serializer => ShopSimpleSerializer
+  end
+
 end
