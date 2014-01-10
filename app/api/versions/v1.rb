f4fa4ac6b319f567a7b6e5cadd473d054c335483
tonilin <tonilin@gmail.com>
2014-01-10 16:36:26 +0800

    implements the api

---
+++ app/api/versions/v1.rb
@@ -0,0 +1,21 @@
+class Versions::V1 < Grape::API
+  version 'v1', :using => :path
+
+
+
+  resource :shops do
+
+
+    desc "Return Shops"
+    params do
+      optional :per_page, :type => Integer, :default => 100
+      optional :page, :type => Integer, :default => 1
+    end
+    get do
+      @shops = Shop.recent.paginate(:page => params[:page], :per_page => params[:per_page])
+
+      present @shops, :with => Entities::Shops
+    end
+  end
+
+end
