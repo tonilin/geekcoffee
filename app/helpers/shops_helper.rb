13a4db6374fad2ee9ac43c3139fe3b5e71f8ba16
tonilin <tonilin@gmail.com>
2014-01-12 16:38:47 +0800

    helper to render the shop name

--- app/helpers/shops_helper.rb
+++ app/helpers/shops_helper.rb
@@ -1,4 +1,14 @@
 module ShopsHelper
 
+  def render_shop_name(shop, link: true)
+    Rails.logger.info(link.to_json)
+
+    if link
+      link_to(shop.name, shop_path(shop))
+    else
+      shop.name
+    end
+  end
+
 
 end
