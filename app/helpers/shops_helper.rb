bee34ddb33300b87e6c2b6d1c961f3d096951774
tonilin <tonilin@gmail.com>
2014-01-12 16:58:25 +0800

    render shop show

--- app/helpers/shops_helper.rb
+++ app/helpers/shops_helper.rb
@@ -8,5 +8,29 @@ module ShopsHelper
     end
   end
 
+  def render_shop_address(shop)
+    shop.address
+  end
+
+  def render_shop_website(shop)
+    link_to(shop.website_url, shop.website_url)
+  end
+
+  def render_shop_hours(shop)
+    shop.hours
+  end
+
+  def render_shop_if_wifi_free(shop)
+    shop.is_wifi_free ? fa_icon("check") : fa_icon("times")
+  end
+
+  def render_shop_has_power_outlets(shop)
+    shop.power_outlets ? fa_icon("check") : fa_icon("times")
+  end
+
+  def render_shop_description(shop)
+    shop.description
+  end
+
 
 end
