8275c8ce0d959d32f95c348cacf5aa6261ae491a
tonilin <tonilin@gmail.com>
2014-01-15 10:53:49 +0800

    debug

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 if Rails.env.production?
-  MyApplication::Application.config.session_store :redis_store, key: '_geekcoffee_session'
+  Geekcoffee::Application.config.session_store :redis_store, key: '_geekcoffee_session'
 else
   Geekcoffee::Application.config.session_store :cookie_store, key: '_geekcoffee_session'
 end
