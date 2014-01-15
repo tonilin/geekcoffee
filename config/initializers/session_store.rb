f95b7f54932a77b91487c32ea4abc517c41ec00a
tonilin <tonilin@gmail.com>
2014-01-15 10:51:00 +0800

    using redis in production

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
-Geekcoffee::Application.config.session_store :cookie_store, key: '_geekcoffee_session'
+if Rails.env.production?
+  MyApplication::Application.config.session_store :redis_store, key: '_geekcoffee_session'
+else
+  Geekcoffee::Application.config.session_store :cookie_store, key: '_geekcoffee_session'
+end
+
+
