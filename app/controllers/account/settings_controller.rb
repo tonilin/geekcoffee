ff49cf2d64ef833f718b1a45d5b4296760a9613f
tonilin <tonilin@gmail.com>
2014-01-17 20:50:16 +0800

    settings controller

--- app/controllers/account/settings_controller.rb
+++ app/controllers/account/settings_controller.rb
@@ -5,7 +5,17 @@ class Account::SettingsController < AuthenticatedController
   end
 
   def create
+    if current_user.update(user_params)
+      redirect_to :back
+    else
+      render :index
+    end
 
+  end
+
+
+  def user_params
+    params.require(:user).permit(:name)
   end
 
 
