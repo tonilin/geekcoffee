ab7e2425213c29f3a51604d51e1b9fe1ca22e41e
tonilin <tonilin@gmail.com>
2014-01-17 21:01:51 +0800

    add notice msg

--- app/controllers/account/settings_controller.rb
+++ app/controllers/account/settings_controller.rb
@@ -6,7 +6,7 @@ class Account::SettingsController < AuthenticatedController
 
   def create
     if current_user.update(user_params)
-      redirect_to :back
+      redirect_to :back, :notice => "成功更新資料"
     else
       render :index
     end
