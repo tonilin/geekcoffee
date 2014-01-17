b5df7bf03981221fb7926ea46a1728094073fe4e
tonilin <tonilin@gmail.com>
2014-01-17 21:00:19 +0800

    override

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Geekcoffee::Application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :registrations => "registrations"  }
 
   mount API => '/api'
 
