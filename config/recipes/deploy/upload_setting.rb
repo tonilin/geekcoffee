1d242b6625898207dc035578ba0d9a695bc959ea
tonilin <tonilin@gmail.com>
2014-01-18 12:07:40 +0800

    fix upload setting recipe

--- config/recipes/deploy/upload_setting.rb
+++ config/recipes/deploy/upload_setting.rb
@@ -2,6 +2,7 @@ namespace :deploy do
   
   desc "upload application.yml to server"
   task :upload_setting do
+    run "mkdir -p #{deploy_to}/shared/config"
     transfer :up, "config/application.yml", "#{shared_path}/config/application.yml", :via => :scp
   end
 
