2879fa6903755d991d3a5db64a27d43368f29f5f
tonilin <tonilin@gmail.com>
2014-01-18 11:59:55 +0800

    debug

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,24 +32,6 @@ set :hipchat_announce, false # notify users?
 
 set :scm_verbose, true
 
-namespace :sitemap do
-
-  desc "refresh sitemap"
-  task :refresh do
-    run "cd #{latest_release} && RAILS_ENV=#{rails_env} bundle exec rake sitemap:refresh"
-  end
-
-  desc "refresh sitemap without ping"
-  task :refresh_without_ping do
-    run "cd #{latest_release} && RAILS_ENV=#{rails_env} bundle exec rake -s sitemap:refresh"
-  end
-
-  desc "copy_old_sitemap"
-  task :copy_old_sitemap do
-    run "if [ -e #{previous_release}/public/sitemap.xml.gz ]; then cp #{previous_release}/public/sitemap*.xml.gz #{current_release}/public/; fi"
-  end
-end
-
 
 namespace :my_tasks do
   task :symlink, :roles => [:web] do
