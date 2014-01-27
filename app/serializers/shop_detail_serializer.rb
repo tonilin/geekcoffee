0dfc08b543dd2d5aaae906370e6de5c687e3480e
tonilin <tonilin@gmail.com>
2014-01-27 14:48:11 +0800

    refine

--- app/serializers/shop_detail_serializer.rb
+++ app/serializers/shop_detail_serializer.rb
@@ -1,5 +1,5 @@
 class ShopDetailSerializer < ActiveModel::Serializer
-  attributes :id, :slug, :name, :lat, :lng, :description, :is_wifi_free, :power_outlets, :website_url, :avg_rating, :cover_thumb_url
+  attributes :id, :slug, :name, :lat, :lng, :description, :is_wifi_free, :power_outlets, :website_url, :avg_rating, :address, :cover_thumb_url
 
 
   def cover_thumb_url
