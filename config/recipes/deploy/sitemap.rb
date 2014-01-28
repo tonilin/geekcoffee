1ef756651f1446521873ca925bf968e5a88d2ee1
tonilin <tonilin@gmail.com>
2014-01-28 17:05:32 +0800

    add sitemap namepsace

--- config/recipes/deploy/sitemap.rb
+++ config/recipes/deploy/sitemap.rb
@@ -1,17 +1,18 @@
 namespace :deploy do
+  namespace :sitemap do
+    desc "refresh sitemap"
+    task :refresh do
+      run "cd #{latest_release} && RAILS_ENV=#{rails_env} bundle exec rake sitemap:refresh"
+    end
 
-  desc "refresh sitemap"
-  task :refresh do
-    run "cd #{latest_release} && RAILS_ENV=#{rails_env} bundle exec rake sitemap:refresh"
-  end
+    desc "refresh sitemap without ping"
+    task :refresh_without_ping do
+      run "cd #{latest_release} && RAILS_ENV=#{rails_env} bundle exec rake -s sitemap:refresh"
+    end
 
-  desc "refresh sitemap without ping"
-  task :refresh_without_ping do
-    run "cd #{latest_release} && RAILS_ENV=#{rails_env} bundle exec rake -s sitemap:refresh"
-  end
-
-  desc "copy_old_sitemap"
-  task :copy_old_sitemap do
-    run "if [ -e #{previous_release}/public/sitemap.xml.gz ]; then cp #{previous_release}/public/sitemap*.xml.gz #{current_release}/public/; fi"
+    desc "copy_old_sitemap"
+    task :copy_old_sitemap do
+      run "if [ -e #{previous_release}/public/sitemap.xml.gz ]; then cp #{previous_release}/public/sitemap*.xml.gz #{current_release}/public/; fi"
+    end
   end
 end
