b1f30dc82f33b81a0f5e4d6212e05dca76d5efb2
tonilin <tonilin@gmail.com>
2014-01-28 17:09:10 +0800

    debug

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,6 +7,6 @@ role :web, "geekcoffee.roachking.net"                   # Your HTTP server, Apac
 role :app, "geekcoffee.roachking.net"                   # This may be the same as your `Web` server
 role :db,  "geekcoffee.roachking.net", :primary => true # This is where Rails migrations will run
 
-after "deploy:update_code", "deploy:copy_old_sitemap"
+after "deploy:update_code", "deploy:sitemap:copy_old_sitemap"
 after 'deploy:restart', 'unicorn:reload' # app IS NOT preloaded
 after 'deploy:restart', 'unicorn:restart'  # app preloaded
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -8,6 +8,6 @@ role :web, "geekcoffee-staging.roachking.net"                   # Your HTTP serv
 role :app, "geekcoffee-staging.roachking.net"                   # This may be the same as your `Web` server
 role :db,  "geekcoffee-staging.roachking.net", :primary => true # This is where Rails migrations will run
 
-after "deploy:update_code", "deploy:copy_old_sitemap"
+after "deploy:update_code", "deploy:sitemap:copy_old_sitemap"
 after 'deploy:restart', 'unicorn:reload' # app IS NOT preloaded
 after 'deploy:restart', 'unicorn:restart'  # app preloaded
