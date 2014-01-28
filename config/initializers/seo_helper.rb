f84556e64451119ab8c094d4fffb0ea169c7fc7a
tonilin <tonilin@gmail.com>
2014-01-28 10:47:56 +0800

    add seo keywords

--- config/initializers/seo_helper.rb
+++ config/initializers/seo_helper.rb
@@ -3,7 +3,7 @@ SeoHelper.configure do |config|
   config.skip_blank               = false
   config.site_name = Setting.app_name
   config.default_page_description = "Geek Coffee, 尋找你附近的咖啡廳"
-  config.default_page_keywords    = "Geek, wifi, cafe, coffee, free"
+  config.default_page_keywords    = "咖啡館, 咖啡, 上網, 咖啡店, 喝咖啡, Geek Coffee,Geek, wifi, cafe, coffee, free"
   config.default_page_image = Setting.domain + Setting.default_logo_url
   config.site_name_formatter  = lambda { |title, site_name|   "#{title} « #{site_name}".html_safe }
   config.pagination_formatter = lambda { |title, page_number| "#{title} - Page No.#{page_number}" }
