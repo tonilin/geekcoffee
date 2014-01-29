4050112094c84644ce8e4ea8894a27f7719fc408
tonilin <tonilin@gmail.com>
2014-01-29 16:43:40 +0800

    add cliend method

--- app/models/foursquare.rb
+++ app/models/foursquare.rb
@@ -1,2 +1,12 @@
 class Foursquare < ActiveRecord::Base
+
+
+
+  def self.client
+    Foursquare2::Client.new(
+      :client_id => Setting.foursquare_id,
+      :client_secret => Setting.foursquare_secret
+    )
+  end
+
 end
